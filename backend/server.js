bc14df9f386b8faee24c4791b6c02b3575f8a413
Ambeeru Vignesh <vigneshvj53@gmail.com>
2023-05-07 22:13:17 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import connectDB from "./config/db";
 
+connectDB();
 
-const app=express();
+const app = express();
 dotenv.config();
 
 app.use(express.json());
 
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("App is running");
+});
